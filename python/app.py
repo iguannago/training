f61f8187e2d1f0dbc8bb2a9bd8d6058e428f6cc1
david.crespo <david.crespo@hmcts.net>
2023-02-22 10:15:41 +0000

    first python program

---
+++ python/app.py
@@ -0,0 +1,11 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def greet(self):
+        print("Hello, my name is " + self.name + " and I am " + str(self.age))
+
+
+person = Person("David", 44)
+person.greet()
