dddf1500e48d0973d635ec26c2bb8202c418e07b
david.crespo <david.crespo@hmcts.net>
2023-02-23 13:02:55 +0000

    working with useEffects

--- Ex_Files_React_js_EssT/react-app/src/App.js
+++ Ex_Files_React_js_EssT/react-app/src/App.js
@@ -1,21 +1,38 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App({ firstname }) {
   const [emotion, setEmotion] = useState('happy');
+  const [secondary, setSecondary] = useState('tired');
+
+  useEffect(() => {
+    console.log(`It's ${emotion} right now`);
+  }, [emotion]);
+
+  useEffect(() => {
+    console.log(`It's ${secondary} around here`);
+  }, [secondary]);
 
   return (
     <div className='App'>
       <h1>Hello {firstname}</h1>
-      <h2>Your emotion is:</h2>
-      <h1>{emotion}</h1>
+      <h2>Your emotion is: {emotion}</h2>
       <button
         onClick={() => {
           if (emotion === 'happy') setEmotion('sad');
           else setEmotion('happy');
         }}
       >
-        change emotion
+        Change emotion
+      </button>
+      <h2>Current secondary emotion is: {secondary}</h2>
+      <button
+        onClick={() => {
+          if (secondary === 'tired') setSecondary('grateful');
+          else setSecondary('tired');
+        }}
+      >
+        Change secondary emotion
       </button>
     </div>
   );
