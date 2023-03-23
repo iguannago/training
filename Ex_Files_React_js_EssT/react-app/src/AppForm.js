f387de8b0742e3bdfe00422a1c01ecd810ccd230
david.crespo <david.crespo@hmcts.net>
2023-03-23 16:32:57 +0000

    playing a bit more with useState  and the github api

--- Ex_Files_React_js_EssT/react-app/src/AppForm.js
+++ Ex_Files_React_js_EssT/react-app/src/AppForm.js
@@ -1,9 +1,10 @@
 import './App.css';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 function AppForm() {
   const colorTitle = useRef();
   const hexColor = useRef();
+  const [userData, setUserData] = useState({});
 
   const submit = (e) => {
     e.preventDefault();
@@ -12,14 +13,47 @@ function AppForm() {
     alert(title + ' ' + color);
     colorTitle.current.value = '';
     hexColor.current.value = '';
+    console.log(title, color);
   };
   return (
-    <form onSubmit={submit}>
-      <input ref={colorTitle} type='text' placeholder='color title...' />
-      <input ref={hexColor} type='color' />
-      <button>ADD</button>
-    </form>
+    <div className='App'>
+      <h1>Color Organizer</h1>
+      <form onSubmit={submit}>
+        <input ref={colorTitle} type='text' placeholder='color title...' />
+        <input ref={hexColor} type='color' />
+        <button>ADD</button>
+      </form>
+      <div>
+        <button
+          onClick={() => {
+            userData.name === undefined ? fetchUserData() : setUserData({});
+          }}
+        >
+          More about me
+        </button>
+        {userData.name !== undefined && (
+          <div>
+            <h2>{userData.name}</h2>
+            <p>Username: {userData.login}</p>
+            <p>Location: {userData.location}</p>
+            <p>Bio: {userData.bio}</p>
+            <img src={userData.avatar_url} alt={userData.name} />
+          </div>
+        )}
+      </div>
+    </div>
   );
+
+  async function fetchUserData() {
+    try {
+      const response = await fetch('https://api.github.com/users/iguannago');
+      const data = await response.json();
+      console.log(data);
+      setUserData(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
 
 export default AppForm;
