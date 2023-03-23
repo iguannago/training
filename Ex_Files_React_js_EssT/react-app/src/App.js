13205cb17d858382fbfaadbee759bfc485d472ac
david.crespo <david.crespo@hmcts.net>
2023-03-23 11:45:22 +0000

    useReducer

--- Ex_Files_React_js_EssT/react-app/src/App.js
+++ Ex_Files_React_js_EssT/react-app/src/App.js
@@ -1,26 +1,26 @@
 import './App.css';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useReducer } from 'react';
 
 function App({ firstname }) {
   const [emotion, setEmotion] = useState('happy');
   const [secondary, setSecondary] = useState('tired');
+  const [checked, setChecked] = useReducer((checked) => !checked, false);
 
   useEffect(() => {
     console.log(`It's ${emotion} right now`);
-  }, [emotion]);
-
-  useEffect(() => {
-    console.log(`It's ${secondary} around here`);
-  }, [secondary]);
+    console.log(`It's ${secondary} right now`);
+    console.log(`It's ${checked} right now`);
+  }, [emotion, secondary, checked]);
 
   return (
     <div className='App'>
       <h1>Hello {firstname}</h1>
+      <input type='checkbox' value={checked} onChange={setChecked} />
+      <label>{checked ? 'checked' : 'not checked'}</label>
       <h2>Your emotion is: {emotion}</h2>
       <button
         onClick={() => {
-          if (emotion === 'happy') setEmotion('sad');
-          else setEmotion('happy');
+          setEmotion((emotion) => (emotion === 'happy' ? 'sad' : 'happy'));
         }}
       >
         Change emotion
@@ -28,8 +28,9 @@ function App({ firstname }) {
       <h2>Current secondary emotion is: {secondary}</h2>
       <button
         onClick={() => {
-          if (secondary === 'tired') setSecondary('grateful');
-          else setSecondary('tired');
+          setSecondary((secondary) =>
+            secondary === 'tired' ? 'happy' : 'tired'
+          );
         }}
       >
         Change secondary emotion
