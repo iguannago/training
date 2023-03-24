8c84d65231bbae12395c75902c25f1c298acf2a3
david.crespo <david.crespo@hmcts.net>
2023-03-24 15:00:24 +0000

    extend class and implement interface examples

--- python/app.py
+++ python/app.py
@@ -9,3 +9,45 @@ class Person:
 
 person = Person("David", 44)
 person.greet()
+
+# define class which extends Person
+class Employee(Person):
+    def __init__(self, name, age, salary):
+        super().__init__(name, age)
+        self.salary = salary
+
+    def greet(self):
+        print(
+            "Hello, my name is "
+            + self.name
+            + " and I am "
+            + str(self.age)
+            + " and I make "
+            + str(self.salary)
+        )
+
+
+employee = Employee("John", 55, 50000)
+employee.greet()
+
+# define interface
+class IStream:
+    def read(self):
+        raise NotImplementedError
+
+    def write(self, data):
+        raise NotImplementedError
+
+    # define class which implements interface
+
+
+class SocketStream(IStream):
+    def read(self):
+        print("Reading data from socket")
+
+    def write(self, data):
+        print("Writing data to socket")
+
+
+socketStream = SocketStream()
+socketStream.read()
